958388e3c333ff8252576ccf87db11cbb736ac79
mohamedamr77 <moamr947@gmail.com>
2025-01-22 01:19:33 +0200

    finish item widget

---
+++ lib/feature/home/presentation/view/widgets/product_grid.dart
@@ -0,0 +1,24 @@
+import 'package:elevatetask/core/extentions/screen_size.dart';
+import 'package:elevatetask/core/shared_widget/global_text.dart';
+import 'package:elevatetask/core/utils/app_colors.dart';
+import 'package:elevatetask/feature/home/presentation/view/widgets/product_card.dart';
+import 'package:flutter/material.dart';
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      alignment: WrapAlignment.spaceAround,
+      runSpacing: 0.03.h, // Vertical spacing between rows
+      children: List.generate(
+        8,
+            (index) =>  SizedBox(
+            width: 0.43.w, // Set width for each item
+            child:const ProductCard(),
+        ),
+      ),
+    );
+  }
+}
+
