294ebda94a479dabab7243c29f9bf341bf447703
mohamedamr77 <moamr947@gmail.com>
2025-01-21 23:57:18 +0200

    make refecator in bottom nav bar

--- lib/feature/bottom_nav_bar/presentation/view/bottom_nav_bar_view.dart
+++ lib/feature/bottom_nav_bar/presentation/view/bottom_nav_bar_view.dart
@@ -1,13 +1,12 @@
 import 'package:elevatetask/core/extentions/screen_size.dart';
 import 'package:elevatetask/core/utils/font_family.dart';
+import 'package:elevatetask/core/utils/font_size_responsize.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 import '../../../../core/utils/app_colors.dart';
 import '../../data/bottom_nav_icon_list.dart';
 
 class BottomNavBarView extends StatefulWidget {
-  static String id ="BottomNavBarView";
+  static String id = "BottomNavBarView";
   const BottomNavBarView({super.key});
 
   @override
@@ -15,33 +14,40 @@ class BottomNavBarView extends StatefulWidget {
 }
 
 class _BottomNavBarViewState extends State<BottomNavBarView> {
-  var navPages = [
-    const Scaffold(
-      body: Center(child: Text("Amr"),),
-    ),
-     Scaffold(
-       body: Center(child: Text("Mohamed"),),
-     ),
-    // const HomeScreen(),
-    // const DoctorsScreen(),
-    // const VaccineTimesScreen(),
-    // const AccountScreen(),
+  final List<Widget> navPages = [
+    const Scaffold(body: Center(child: Text("Amr"))),
+    const Scaffold(body: Center(child: Text("Mohamed"))),
+    // Add more pages here
   ];
 
-  int localIndex = 0;
-  PageController? controller;
+  int currentIndex = 0;
+  late PageController pageController;
 
   @override
   void initState() {
     super.initState();
-    controller = PageController(initialPage: 0);
+    pageController = PageController(initialPage: currentIndex);
   }
 
   @override
   void dispose() {
-    controller!.dispose();
+    pageController.dispose();
     super.dispose();
   }
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+      pageController.jumpToPage(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,107 +56,98 @@ class _BottomNavBarViewState extends State<BottomNavBarView> {
         child: PageView.builder(
           reverse: true,
           itemCount: navPages.length,
-          controller: controller,
-          onPageChanged: (val) {
-            setState(() {
-              localIndex = val;
-            });
-          },
-          itemBuilder: (context, index) {
-            return navPages[index];
-          },
+          controller: pageController,
+          onPageChanged: onPageChanged,
+          itemBuilder: (context, index) => navPages[index],
         ),
       ),
-      bottomNavigationBar: Container(
-          padding: EdgeInsets.only(top: 0.01.h),
-          decoration: const BoxDecoration(
-            color: AppColors.skyBlueColor,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      padding: EdgeInsets.only(top: 0.01.h),
+      decoration: const BoxDecoration(
+        color: AppColors.skyBlueColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(12),
+          topRight: Radius.circular(12),
+        ),
+      ),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: onTabTapped,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: AppColors.primaryColor,
+          unselectedItemColor: AppColors.mediumGrayColor.withOpacity(0.7),
+          selectedLabelStyle:  TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: getResponsiveFontSize(context: context, fontSize: 16),
+            fontFamily: AppFontFamily.cairoFontFamily,
+          ),
+          unselectedLabelStyle:  TextStyle(
+            fontSize: getResponsiveFontSize(context: context, fontSize: 14),
+            fontFamily: AppFontFamily.cairoFontFamily,
+          ),
+          enableFeedback: false,
+          items: List.generate(
+            navPages.length,
+            (index) => BottomNavigationBarItem(
+              icon: _buildAnimatedIcon(index),
+              label: bottomNavIconsList[index].label,
             ),
           ),
-          child: Theme(
-            data: Theme.of(context).copyWith(
-              splashColor: Colors.transparent, // Remove ripple effect
-              highlightColor: Colors.transparent, // Remove highlight effect
-            ),
-            child: BottomNavigationBar(
-              currentIndex: localIndex,
-              onTap: (index) {
-                setState(() {
-                  localIndex = index;
-                  controller!.jumpToPage(index);
-                });
-              },
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              type: BottomNavigationBarType.fixed,
-              selectedItemColor: AppColors.primaryColor,
-              unselectedItemColor: AppColors.mediumGrayColor.withOpacity(0.7),
-              selectedLabelStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                fontFamily: AppFontFamily.cairoFontFamily,
-              ),
-              unselectedLabelStyle: const TextStyle(
-                fontSize: 14,
-                fontFamily: AppFontFamily.cairoFontFamily,
-              ),
-              enableFeedback: false,
-              items: List.generate(
-                navPages.length,
-                    (index) => BottomNavigationBarItem(
-                  icon: AnimatedBuilder(
-                    animation: Listenable.merge([controller!]),
-                    builder: (context, _) {
-                      // Check if the current item is selected
-                      final isActive = localIndex == index;
+        ),
+      ),
+    );
+  }
+  Widget _buildAnimatedIcon(int index) {
+    final isActive = currentIndex == index;
 
-                      return Stack(
-                        clipBehavior: Clip.none,
-                        alignment: Alignment.center,
-                        children: [
-                          AnimatedPositioned(
-                            duration: const Duration(milliseconds: 300),
-                            top: isActive ? -40 : 0, // Active item moves upward
-                            child: AnimatedContainer(
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeInOut,
-                              width: isActive
-                                  ? 50
-                                  : 0, // Circle appears only for active item
-                              height: isActive ? 50 : 0,
-                              decoration: BoxDecoration(
-                                color: isActive
-                                    ? AppColors.darkBlueAccent
-                                    : Colors.transparent,
-                                shape: BoxShape.circle,
-                              ),
-                            ),
-                          ),
-                          AnimatedContainer(
-                            duration: const Duration(milliseconds: 300),
-                            transform: Matrix4.translationValues(
-                              0,
-                              isActive
-                                  ? -25
-                                  : 0, // Icon moves upward when active
-                              0,
-                            ),
-                            child: isActive
-                                ? bottomNavIconsList[index].iconSelected
-                                : bottomNavIconsList[index].iconUnSelected,
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                  label: bottomNavIconsList[index].label,
+    return AnimatedBuilder(
+      animation: Listenable.merge([pageController]),
+      builder: (context, _) {
+        return Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.center,
+          children: [
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 300),
+              top: isActive ? -40 : 0,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                width: isActive ? 50 : 0,
+                height: isActive ? 50 : 0,
+                decoration: BoxDecoration(
+                  color:
+                      isActive ? AppColors.darkBlueAccent : Colors.transparent,
+                  shape: BoxShape.circle,
                 ),
               ),
             ),
-          )),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              transform: Matrix4.translationValues(
+                0,
+                isActive ? -25 : 0,
+                0,
+              ),
+              child: isActive
+                  ? bottomNavIconsList[index].iconSelected
+                  : bottomNavIconsList[index].iconUnSelected,
+            ),
+          ],
+        );
+      },
     );
   }
 }
