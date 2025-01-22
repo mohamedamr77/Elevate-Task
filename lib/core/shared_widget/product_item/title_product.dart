868b3f9b0834a9bd90048d57f45e27079871382b
mohamedamr77 <moamr947@gmail.com>
2025-01-22 19:55:32 +0200

    solve issue

--- lib/core/shared_widget/product_item/title_product.dart
+++ lib/core/shared_widget/product_item/title_product.dart
@@ -11,12 +11,17 @@ class TitleProduct extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: GText(
-        color: AppColors.primaryColor,
-        content: title,
-        fontSize: 16,
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
+      child: SizedBox(
+        height: 40,
+        child: Center(
+          child: GText(
+            color: AppColors.primaryColor,
+            content: title,
+            fontSize: 16,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
       ),
     );
   }
