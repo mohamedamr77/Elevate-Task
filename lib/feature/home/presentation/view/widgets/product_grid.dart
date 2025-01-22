868f28881afa2abc22477457b044abb8979a3940
mohamedamr77 <moamr947@gmail.com>
2025-01-22 04:11:21 +0200

    finish home

--- lib/feature/home/presentation/view/widgets/product_grid.dart
+++ lib/feature/home/presentation/view/widgets/product_grid.dart
@@ -19,62 +19,92 @@ class _ProductGridState extends State<ProductGrid>
   @override
   void initState() {
     super.initState();
+    _fetchProducts();
+  }
+
+  void _fetchProducts() {
     BlocProvider.of<HomeCubit>(context).fetchProduct();
   }
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    var cubit = BlocProvider.of<HomeCubit>(context);
-
+    super.build(context); // Required for AutomaticKeepAliveClientMixin
     return BlocConsumer<HomeCubit, HomeState>(
       builder: (BuildContext context, state) {
-        List<ProductModel>? products = cubit.products;
-
-        if (state is ProductsHomeLoadingState && products.isEmpty) {
-          return const BuildShimmerShape(width: 100, height: 100);
-        }
-
-        if (state is ProductsHomeFaliureState) {
-          return Center(
-            child: Text(
-              state.errorMessage,
-              style: const TextStyle(
-                color: Colors.red,
-                fontSize: 16,
-              ),
-            ),
-          );
-        }
-
-        if ( state is ProductsHomeSuccessState) {
-          return  Wrap(
-            alignment: WrapAlignment.spaceAround,
-            runSpacing: 0.03.h, // Vertical spacing between rows
-            children: List.generate(
-              products.length,
-                  (index) => SizedBox(
-                width: 0.43.w, // Set width for each item
-                child: ProductCard(productModel: products[index]),
-              ),
-            ),
-          );
-        }
-
-        return const SizedBox();
+        return _buildContent(state);
       },
       listener: (BuildContext context, Object? state) {
-        if (state is ProductsHomeFaliureState) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(state.errorMessage),
-              backgroundColor: Colors.red,
-            ),
-          );
-        }
+        _handleStateChanges(state);
       },
     );
   }
+
+  Widget _buildContent(HomeState state) {
+    final cubit = BlocProvider.of<HomeCubit>(context);
+    final products = cubit.products;
+
+    if (state is ProductsHomeLoadingState && products.isEmpty) {
+      return _buildLoadingState();
+    }
+
+    if (state is ProductsHomeFaliureState) {
+      return _buildErrorState(state.errorMessage);
+    }
+
+    if (state is ProductsHomeSuccessState) {
+      return _buildProductGrid(products);
+    }
+
+    return const SizedBox();
+  }
+
+  Widget _buildLoadingState() {
+    return Wrap(
+      alignment: WrapAlignment.spaceAround,
+      runSpacing: 0.03.h,
+      children:List.generate(10,
+          (index) =>BuildShimmerShape(width: 0.44.w, height: 0.3.h)),
+    );
+  }
+
+  Widget _buildErrorState(String errorMessage) {
+    return Center(
+      child: Text(
+        errorMessage,
+        style: const TextStyle(
+          color: Colors.red,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProductGrid(List<ProductModel> products) {
+
+    return Wrap(
+      alignment: WrapAlignment.spaceAround,
+      runSpacing: 0.03.h, // Vertical spacing between rows
+      children: List.generate(
+        products.length,
+            (index) => SizedBox(
+          width: 0.43.w, // Set width for each item
+          child: ProductCard(productModel: products[index]),
+        ),
+      ),
+    );
+  }
+
+  void _handleStateChanges(Object? state) {
+    if (state is ProductsHomeFaliureState) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(state.errorMessage),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
   @override
   bool get wantKeepAlive => true; // Preserve the state of this widget
 }
